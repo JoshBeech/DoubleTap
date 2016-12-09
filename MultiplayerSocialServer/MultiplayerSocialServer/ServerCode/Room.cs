e8c1b9294592e9f61b145d2c89fded6a9c56cf39
Josh Beech <jwbeech@btinternet.com>
2016-12-09 23:33:41 +0000

    Added basic UDP listener and most TCP Listener to separate class

--- MultiplayerSocialServer/MultiplayerSocialServer/ServerCode/Room.cs
+++ MultiplayerSocialServer/MultiplayerSocialServer/ServerCode/Room.cs
@@ -11,20 +11,30 @@ namespace MultiplayerSocialServer
 	public class Room
 	{
 		public string name;
+		public int RoomID;
 		public Dictionary<string, Socket> Clients = new Dictionary<string, Socket>();
-		public int UdpPort; 
-		public UdpClient UdpListener;
-		public IPEndPoint RoomUdpEndPoint;
+		public int UDPPort; 
 
-		public Room(string p_RoomName, int p_UdpPort)
+		public Room(string p_RoomName, int p_RoomID, int p_UDPPort)
 		{
 			name = p_RoomName;
-			UdpPort = p_UdpPort;
-			UdpListener = new UdpClient(UdpPort);
-			RoomUdpEndPoint = new IPEndPoint (IPAddress.Any, UdpPort);
+			RoomID = p_RoomID;
+			UDPPort = p_UDPPort;
+
+			UDPAsynchronousListener.StartListening((UDPPort + RoomID));
 		}
 
+		public void AddClient(string p_ClientName, Socket p_ClientSocket)
+		{
+			Clients.Add(p_ClientName, p_ClientSocket);
+			// Add the client to the UDP connection/broadcast?
+		}
 
+		public void RemoveClient(string p_ClientName)
+		{
+			Clients.Remove(p_ClientName);
+			// Remove the client from the UDP connection/broadcast?
+		}
 	}
 }
 
---
+++ MultiplayerSocialServer/MultiplayerSocialServer/ServerCode/UDPAsynchronousListener.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace MultiplayerSocialServer
+{
+    public class UDPState
+    {
+        public IPEndPoint EndPoint;
+        public UdpClient client;
+
+        public UDPState(IPEndPoint p_EndPoint, UdpClient p_Client)
+        {
+            EndPoint = p_EndPoint;
+            client = p_Client;
+        }
+    }
+
+    public class UDPAsynchronousListener
+    {
+        public static int port;
+
+        public static void StartListening(int p_Port)
+        {   
+            port = p_Port;
+
+            // Begin Receive
+            ReceiveMessages();
+        }
+
+        public static void ReceiveCallBack(IAsyncResult ar)
+        {
+            UdpClient client = (UdpClient)((UDPState)(ar.AsyncState)).client;
+            IPEndPoint EndPoint = (IPEndPoint)((UDPState)(ar.AsyncState)).EndPoint;
+
+            Byte[] BytesReceived = client.EndReceive(ar, ref EndPoint);
+            string StringReceived = Encoding.ASCII.GetString((BytesReceived));
+
+            Console.WriteLine("Received: {0}", StringReceived);
+
+            // Do the thing (while loop?)
+            SendMessage(EndPoint, StringReceived);
+        }
+
+        public static void ReceiveMessages()
+        {
+            IPEndPoint l_EndPoint = new IPEndPoint(IPAddress.Any, port);
+            UDPState l_state = new UDPState(l_EndPoint, new UdpClient(l_EndPoint));
+
+            l_state.client.BeginReceive(new AsyncCallback(ReceiveCallBack), l_state);
+        }
+
+        public static void SendCallBack(IAsyncResult ar)
+        {
+            UdpClient client = (UdpClient)ar.AsyncState;
+
+            Console.WriteLine("number of bytes sent: {0}", client
+                .EndSend(ar));
+        }
+
+        public static void SendMessage(IPEndPoint p_EndPoint, string message)
+        {
+            UdpClient client = new UdpClient();
+
+            // Was string and port
+            client.Connect(p_EndPoint);
+            Byte[] SendBytes = Encoding.ASCII.GetBytes(message);
+
+            // send message, destination defined by Connect() call
+            client.BeginSend(SendBytes, SendBytes.Length, new AsyncCallback(SendCallBack), client);
+
+            // the while loop thing? talk to julien?
+        }
+    }
+}
+
